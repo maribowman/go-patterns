f67c8b347ba40e1d3c872cbe9f293c7c13ee2b53
Marius B <marius.baumann@mail.schwarz>
2022-11-21 11:12:30 +0100

    impl done hook

--- fan_out_fan_in.go
+++ fan_out_fan_in.go
@@ -2,37 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sync"
+	"time"
 )
 
-const workerCount = 5
+const workerCount = 3
 
 func main() {
-	input := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-
 	inputChannel := make(chan int)
 	doneChannel := make(chan struct{})
-	defer close(doneChannel)
+
+	go func() {
+		time.Sleep(750 * time.Millisecond)
+		// kill process
+		//doneChannel <- struct{}{}
+		close(doneChannel)
+		fmt.Println("closed doneChannel")
+	}()
 
 	go func() {
 		defer close(inputChannel)
 
-		for _, data := range input {
+		for i := 0; i < math.MaxInt; i++ {
 			select {
 			case <-doneChannel: // trigger done
+				fmt.Println("killing main input")
+				time.Sleep(50 * time.Millisecond)
 				return
-			case inputChannel <- data:
+			case inputChannel <- i:
 			}
 		}
 	}()
 
-	channels := fanOut(doneChannel, inputChannel)
-	mergeChannel := fanIn(doneChannel, channels...)
+	workerChannel := fanOut(doneChannel, inputChannel)
+	mergeChannel := fanIn(doneChannel, workerChannel...)
 
-	var index int
 	for result := range randomOperation(doneChannel, mergeChannel) {
-		index++
-		fmt.Println(fmt.Sprintf("run %d: %d", index, result))
+		if result%100000 == 0 {
+			fmt.Println(fmt.Sprintf("run %d", result))
+		}
 	}
 }
 
@@ -43,41 +52,45 @@ func fanOut(doneChannel chan struct{}, inputChannel chan int) []chan int {
 		workerChannel := make(chan int)
 		resultChannels[i] = workerChannel
 
-		go func() {
+		go func(workerId int) {
 			defer close(workerChannel)
 
 			for data := range inputChannel {
 				result := data + 1 // actual operation
 				select {
-				case <-doneChannel: // shutting down fan-out
+				case <-doneChannel:
+					fmt.Println(fmt.Sprintf("fan-out: shutting down worker#%d", workerId))
+					time.Sleep(50 * time.Millisecond)
 					return
 				case workerChannel <- result:
 				}
 			}
-		}()
+		}(i)
 	}
 
 	return resultChannels
 }
 
-func fanIn(doneChannel chan struct{}, resultChannels ...chan int) chan int {
+func fanIn(doneChannel chan struct{}, workerChannel ...chan int) chan int {
 	mergeChannel := make(chan int)
 	var wg sync.WaitGroup
 
-	for _, channel := range resultChannels {
+	for channelId, channel := range workerChannel {
 		wg.Add(1)
 		channelClosure := channel
 
-		go func() {
+		go func(channelId int) {
 			defer wg.Done()
 			for data := range channelClosure {
 				select {
 				case <-doneChannel: // shutting down fan-in
+					fmt.Println(fmt.Sprintf("fan-in: shutting down workerChannel#%d", channelId))
+					time.Sleep(50 * time.Millisecond)
 					return
 				case mergeChannel <- data:
 				}
 			}
-		}()
+		}(channelId)
 	}
 
 	go func() {
@@ -98,6 +111,8 @@ func randomOperation(doneChannel chan struct{}, inputChannel chan int) chan int 
 			result := data - 1 // actual operation
 			select {
 			case <-doneChannel: // exiting random operation
+				fmt.Println("shutting operation")
+				time.Sleep(50 * time.Millisecond)
 				return
 			case resultChannel <- result:
 			}
